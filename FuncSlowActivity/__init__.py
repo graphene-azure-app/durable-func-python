81ee2f9bf3a2fe3783e571096cf1ffc191811f72
Will Li <3086886+willlimtc@users.noreply.github.com>
2021-04-04 22:25:01 -0700

    Initial commit for the demo func

---
+++ FuncSlowActivity/__init__.py
@@ -0,0 +1,14 @@
+# This function is not intended to be invoked directly. Instead it will be
+# triggered by an orchestrator function.
+# Before running this sample, please:
+# - create a Durable orchestration function
+# - create a Durable HTTP starter function
+# - add azure-functions-durable to requirements.txt
+# - run pip install -r requirements.txt
+
+import logging
+import time
+
+def main(name: str) -> str:
+    time.sleep(30)
+    return f"The slow test {name} has been completed!"
